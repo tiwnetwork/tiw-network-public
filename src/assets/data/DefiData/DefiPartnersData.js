684e09956aa5f7324ba5683a265d54d6b248b93d
Karl Ciryll Manrique <karlciryllmanrique@gmail.com>
2025-04-15 01:18:27 +0800

    Update DefiPartnersData.js

--- src/assets/data/DefiData/DefiPartnersData.js
+++ src/assets/data/DefiData/DefiPartnersData.js
@@ -1,5 +1,5 @@
 // Import your images using the correct paths
-import logo1 from '../../../assets/images/brands/logo1.svg';
+import logo1 from '../../../assets/images/brands/placeholder-logo.png';
 import logo2 from '../../../assets/images/brands/logo2.svg';
 import logo3 from '../../../assets/images/brands/logo3.svg';
 import logo4 from '../../../assets/images/brands/logo4.svg';
@@ -16,18 +16,18 @@ import logo16 from '../../../assets/images/brands/logo16.svg';
 
 export const partnerData = [
     { id: 1, src: logo1, alt: 'partners-logo' },
-    { id: 2, src: logo2, alt: 'partners-logo' },
-    { id: 3, src: logo3, alt: 'partners-logo' },
-    { id: 4, src: logo4, alt: 'partners-logo' },
-    { id: 5, src: logo5, alt: 'partners-logo' },
-    { id: 6, src: logo6, alt: 'partners-logo' },
-    { id: 7, src: logo9, alt: 'partners-logo' },
-    { id: 8, src: logo7, alt: 'partners-logo' },
-    { id: 9, src: logo11, alt: 'partners-logo' },
-    { id: 10, src: logo12, alt: 'partners-logo' },
-    { id: 11, src: logo13, alt: 'partners-logo' },
-    { id: 12, src: logo14, alt: 'partners-logo' },
-    { id: 13, src: logo15, alt: 'partners-logo' },
-    { id: 14, src: logo16, alt: 'partners-logo' },
+    { id: 2, src: logo1, alt: 'partners-logo' },
+    { id: 3, src: logo1, alt: 'partners-logo' },
+    { id: 4, src: logo1, alt: 'partners-logo' },
+    { id: 5, src: logo1, alt: 'partners-logo' },
+    { id: 6, src: logo1, alt: 'partners-logo' },
+    { id: 7, src: logo1, alt: 'partners-logo' },
+    { id: 8, src: logo1, alt: 'partners-logo' },
+    { id: 9, src: logo1, alt: 'partners-logo' },
+    { id: 10, src: logo1, alt: 'partners-logo' },
+    { id: 11, src: logo1, alt: 'partners-logo' },
+    { id: 12, src: logo1, alt: 'partners-logo' },
+    { id: 13, src: logo1, alt: 'partners-logo' },
+    { id: 14, src: logo1, alt: 'partners-logo' },
     { id: 15, src: logo1, alt: 'partners-logo' },
 ];
