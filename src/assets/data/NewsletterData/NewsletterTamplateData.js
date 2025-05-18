43e482b8b59d6a4b01a96ddab2fde6b91cb3cac5
Karl Ciryll <karlciryllmanrique@gmail.com>
2025-05-18 11:00:34 +0800

    Compress Images

--- src/assets/data/NewsletterData/NewsletterTamplateData.js
+++ src/assets/data/NewsletterData/NewsletterTamplateData.js
@@ -6,12 +6,12 @@ import Img4 from '../../../assets/images/tiwmedia/homepage-banner/4.png';
 import Img5 from '../../../assets/images/tiwmedia/homepage-banner/5.png';
 import Img6 from '../../../assets/images/tiwmedia/homepage-banner/6.png';
 import Img7 from '../../../assets/images/tiwmedia/homepage-banner/7.png';
-import Img8 from '../../../assets/images/tiwmedia/homepage-banner/8.png';
-import Img9 from '../../../assets/images/tiwmedia/homepage-banner/9.png';
-import Img10 from '../../../assets/images/tiwmedia/homepage-banner/10.png';
-import Img11 from '../../../assets/images/tiwmedia/homepage-banner/11.png';
-import Img12 from '../../../assets/images/tiwmedia/homepage-banner/12.png';
-import Img13 from '../../../assets/images/tiwmedia/homepage-banner/13.png';
+import Img8 from '../../../assets/images/tiwmedia/homepage-banner/plceholder.png';
+import Img9 from '../../../assets/images/tiwmedia/homepage-banner/plceholder.png';
+import Img10 from '../../../assets/images/tiwmedia/homepage-banner/plceholder.png';
+import Img11 from '../../../assets/images/tiwmedia/homepage-banner/plceholder.png';
+import Img12 from '../../../assets/images/tiwmedia/homepage-banner/plceholder.png';
+import Img13 from '../../../assets/images/tiwmedia/homepage-banner/plceholder.png';
 import Img14 from '../../../assets/images/tiwmedia/homepage-banner/14.png';
 import Img15 from '../../../assets/images/tiwmedia/homepage-banner/15.png';
 import Img16 from '../../../assets/images/tiwmedia/homepage-banner/16.png';
@@ -20,10 +20,10 @@ import Img18 from '../../../assets/images/tiwmedia/homepage-banner/18.png';
 import Img19 from '../../../assets/images/tiwmedia/homepage-banner/19.png';
 import Img20 from '../../../assets/images/tiwmedia/homepage-banner/20.png';
 import Img21 from '../../../assets/images/tiwmedia/homepage-banner/21.png';
-import Img22 from '../../../assets/images/tiwmedia/homepage-banner/22.png';
-import Img23 from '../../../assets/images/tiwmedia/homepage-banner/23.png';
-import Img24 from '../../../assets/images/tiwmedia/homepage-banner/24.png';
-import Img25 from '../../../assets/images/tiwmedia/homepage-banner/25.png';
+import Img22 from '../../../assets/images/tiwmedia/homepage-banner/placeholder.png';
+import Img23 from '../../../assets/images/tiwmedia/homepage-banner/placeholder.png';
+import Img24 from '../../../assets/images/tiwmedia/homepage-banner/placeholder.png';
+import Img25 from '../../../assets/images/tiwmedia/homepage-banner/placeholder.png';
 
 // Define the image paths
 export const imagePaths = [
