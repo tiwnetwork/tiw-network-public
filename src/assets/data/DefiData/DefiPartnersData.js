f0c99d4f846136416057589e8e206fde48e66333
Karl Ciryll <karlciryllmanrique@gmail.com>
2025-07-01 09:48:45 +0800

    HP partners logo upload

--- src/assets/data/DefiData/DefiPartnersData.js
+++ src/assets/data/DefiData/DefiPartnersData.js
@@ -1,33 +1,46 @@
 // Import your images using the correct paths
-import logo1 from '../../../assets/images/brands/placeholder-logo.png';
-import logo2 from '../../../assets/images/brands/logo2.svg';
-import logo3 from '../../../assets/images/brands/logo3.svg';
-import logo4 from '../../../assets/images/brands/logo4.svg';
-import logo5 from '../../../assets/images/brands/logo5.svg';
-import logo6 from '../../../assets/images/brands/logo6.svg';
-import logo7 from '../../../assets/images/brands/logo7.svg';
-import logo9 from '../../../assets/images/brands/logo9.svg';
-import logo11 from '../../../assets/images/brands/logo11.svg';
-import logo12 from '../../../assets/images/brands/logo12.svg';
-import logo13 from '../../../assets/images/brands/logo13.svg';
-import logo14 from '../../../assets/images/brands/logo14.svg';
-import logo15 from '../../../assets/images/brands/logo15.svg';
-import logo16 from '../../../assets/images/brands/logo16.svg';
+import logo1 from '../../../assets/images/tiwmedia/partners/white-logo/1.png';
+import logo2 from '../../../assets/images/tiwmedia/partners/white-logo/2.png';
+import logo3 from '../../../assets/images/tiwmedia/partners/white-logo/3.png';
+import logo4 from '../../../assets/images/tiwmedia/partners/white-logo/4.png';
+import logo5 from '../../../assets/images/tiwmedia/partners/white-logo/5.png';
+import logo6 from '../../../assets/images/tiwmedia/partners/white-logo/6.png';
+import logo7 from '../../../assets/images/tiwmedia/partners/white-logo/7.png';
+import logo8 from '../../../assets/images/tiwmedia/partners/white-logo/8.png';
+import logo9 from '../../../assets/images/tiwmedia/partners/white-logo/9.png';
+import logo10 from '../../../assets/images/tiwmedia/partners/white-logo/10.png';
+import logo11 from '../../../assets/images/tiwmedia/partners/white-logo/11.png';
+import logo12 from '../../../assets/images/tiwmedia/partners/white-logo/12.png';
+import logo13 from '../../../assets/images/tiwmedia/partners/white-logo/13.png';
+import logo14 from '../../../assets/images/tiwmedia/partners/white-logo/14.png';
+import logo15 from '../../../assets/images/tiwmedia/partners/white-logo/15.png';
+import logo16 from '../../../assets/images/tiwmedia/partners/white-logo/16.png';
+import logo17 from '../../../assets/images/tiwmedia/partners/white-logo/17.png';
+import logo18 from '../../../assets/images/tiwmedia/partners/white-logo/18.png';
+import logo19 from '../../../assets/images/tiwmedia/partners/white-logo/19.png';
+import logo20 from '../../../assets/images/tiwmedia/partners/white-logo/20.png';
+import logo21 from '../../../assets/images/tiwmedia/partners/white-logo/21.png';
 
 export const partnerData = [
-    { id: 1, src: logo1, alt: 'partners-logo' },
-    { id: 2, src: logo1, alt: 'partners-logo' },
-    { id: 3, src: logo1, alt: 'partners-logo' },
-    { id: 4, src: logo1, alt: 'partners-logo' },
-    { id: 5, src: logo1, alt: 'partners-logo' },
-    { id: 6, src: logo1, alt: 'partners-logo' },
-    { id: 7, src: logo1, alt: 'partners-logo' },
-    { id: 8, src: logo1, alt: 'partners-logo' },
-    { id: 9, src: logo1, alt: 'partners-logo' },
-    { id: 10, src: logo1, alt: 'partners-logo' },
-    { id: 11, src: logo1, alt: 'partners-logo' },
-    { id: 12, src: logo1, alt: 'partners-logo' },
-    { id: 13, src: logo1, alt: 'partners-logo' },
-    { id: 14, src: logo1, alt: 'partners-logo' },
-    { id: 15, src: logo1, alt: 'partners-logo' },
+    { id: 1, src: logo1, alt: 'Partner Logo 1' },
+    { id: 2, src: logo2, alt: 'Partner Logo 2' },
+    { id: 3, src: logo3, alt: 'Partner Logo 3' },
+    { id: 4, src: logo4, alt: 'Partner Logo 4' },
+    { id: 5, src: logo5, alt: 'Partner Logo 5' },
+    { id: 6, src: logo6, alt: 'Partner Logo 6' },
+    { id: 7, src: logo7, alt: 'Partner Logo 7' },
+    { id: 8, src: logo8, alt: 'Partner Logo 8' },
+    { id: 9, src: logo9, alt: 'Partner Logo 9' },
+    { id: 10, src: logo10, alt: 'Partner Logo 10' },
+    { id: 11, src: logo11, alt: 'Partner Logo 11' },
+    { id: 12, src: logo12, alt: 'Partner Logo 12' },
+    { id: 13, src: logo13, alt: 'Partner Logo 13' },
+    { id: 14, src: logo14, alt: 'Partner Logo 14' },
+    { id: 15, src: logo15, alt: 'Partner Logo 15' },
+    { id: 16, src: logo16, alt: 'Partner Logo 16' },
+    { id: 17, src: logo17, alt: 'Partner Logo 17' },
+    { id: 18, src: logo18, alt: 'Partner Logo 18' },
+    { id: 19, src: logo19, alt: 'Partner Logo 19' },
+    { id: 20, src: logo20, alt: 'Partner Logo 20' },
+    { id: 21, src: logo21, alt: 'Partner Logo 21' },
 ];
